099d276a841093bf3518c38d9961bdf1034f08b3
meric sayan <mericsayan@gmail.com>
2023-10-21 12:06:04 +0300

    ScenarioExecutes -> ScenarioExecutions converted

---
+++ src/main/java/com/faas/core/api/endpoint/controller/operation/details/inquiry/ApiOperationInquiryController.java
@@ -0,0 +1,40 @@
+package com.faas.core.api.endpoint.controller.operation.details.inquiry;
+
+import com.faas.core.api.middleware.operation.details.scenario.ApiOperationScenarioMiddleware;
+import com.faas.core.api.model.ws.operation.details.scenario.ApiOperationScenarioWSModel;
+import com.faas.core.utils.config.ApiRoute;
+import com.faas.core.utils.config.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping(value = AppConstant.API_VERSION + "/api/operation/details/inquiry/")
+public class ApiOperationInquiryController {
+
+
+    @Autowired
+    ApiOperationScenarioMiddleware apiOperationScenarioMiddleware;
+
+
+    @RequestMapping(value = ApiRoute.API_GET_OPERATION_SCENARIOS, method = RequestMethod.POST)
+    public ResponseEntity<?> apiGetOperationScenarios(@RequestParam long agentId,
+                                                      @RequestParam long sessionId,
+                                                      @RequestParam String processId) {
+
+        ApiOperationScenarioWSModel response = apiOperationScenarioMiddleware.apiGetOperationScenarios(agentId,sessionId,processId);
+
+        if (response.getGeneral().getStatus().equalsIgnoreCase(AppConstant.GENERAL_SUCCESS_STATUS)) {
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
+    }
+
+
+
+}
