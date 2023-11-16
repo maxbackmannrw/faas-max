87c2ce2e90ee3cd862a4b194b83e426e376608d6
hyym <mericsayan@gmail.com>
2023-11-16 22:35:16 +0300

    almost finish

--- src/main/java/com/faas/core/base/endpoint/controller/client/remote/details/ClientRemoteDetailsController.java
+++ src/main/java/com/faas/core/base/endpoint/controller/client/remote/details/ClientRemoteDetailsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 @RequestMapping(value = AppConstant.API_VERSION + "/base/client/remote/details/")
 public class ClientRemoteDetailsController {
