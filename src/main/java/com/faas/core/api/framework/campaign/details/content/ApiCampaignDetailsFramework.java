3a39eb5ea186ab6554d884ab74ac9c333b7ebc95
hyym <mericsayan@gmail.com>
2024-01-17 17:52:30 +0300

    almost finish

--- src/main/java/com/faas/core/api/framework/campaign/details/content/ApiCampaignDetailsFramework.java
+++ src/main/java/com/faas/core/api/framework/campaign/details/content/ApiCampaignDetailsFramework.java
@@ -10,11 +10,13 @@ import com.faas.core.base.repo.campaign.details.CampaignAgentRepository;
 import com.faas.core.base.repo.operation.content.OperationRepository;
 import com.faas.core.base.repo.process.content.ProcessRepository;
 import com.faas.core.base.repo.process.details.scenario.ProcessScenarioRepository;
+import com.faas.core.utils.config.AppConstant;
 import com.faas.core.utils.config.AppUtils;
 import com.faas.core.utils.helpers.CampaignHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,8 +58,12 @@ public class ApiCampaignDetailsFramework {
 
     public List<ApiSummaryWSDTO> apiGetCampaignDetailsSummaryService(long agentId, String campaignId) {
 
+        List<ApiSummaryWSDTO> agentCampaignSummary = new ArrayList<>();
+        agentCampaignSummary.add(new ApiSummaryWSDTO(AppConstant.AGENT_ACTIVE_OPERATIONS_SUMMARY, String.valueOf(operationRepository.countByAgentIdAndCampaignIdAndOperationState(agentId,campaignId,AppConstant.ACTIVE_STATE))));
+        agentCampaignSummary.add(new ApiSummaryWSDTO(AppConstant.AGENT_READY_OPERATIONS_SUMMARY,String.valueOf(operationRepository.countByAgentIdAndCampaignIdAndOperationState(agentId,campaignId,AppConstant.READY_STATE ))));
+        agentCampaignSummary.add(new ApiSummaryWSDTO(AppConstant.AGENT_ALL_OPERATIONS_SUMMARY, String.valueOf(operationRepository.countByAgentIdAndCampaignId(agentId,campaignId))));
 
-        return null;
+        return agentCampaignSummary;
     }
 
 
