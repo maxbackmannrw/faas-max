54c3f22cb3c21d903271183c6259c4c4245bd35c
hyym <mericsayan@gmail.com>
2024-02-27 21:20:24 +0300

    almost finish

--- src/main/java/com/faas/core/api/framework/operation/details/channel/call/sip/ApiOperationSipChannelFramework.java
+++ src/main/java/com/faas/core/api/framework/operation/details/channel/call/sip/ApiOperationSipChannelFramework.java
@@ -22,6 +22,7 @@ import java.util.List;
 @Component
 public class ApiOperationSipChannelFramework {
 
+
     @Autowired
     OperationHelper operationHelper;
 
--- src/main/java/com/faas/core/api/framework/operation/details/channel/call/wapp/ApiOperationWappCallChannelFramework.java
+++ src/main/java/com/faas/core/api/framework/operation/details/channel/call/wapp/ApiOperationWappCallChannelFramework.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Component
 public class ApiOperationWappCallChannelFramework {
 
