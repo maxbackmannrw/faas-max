73ea1abd068315d859498299188e787103660eb9
hyym <mericsayan@gmail.com>
2024-03-08 20:43:04 +0300

    almost finish

--- src/main/java/com/faas/core/base/model/db/remote/client/ClientRemoteDBModel.java
+++ src/main/java/com/faas/core/base/model/db/remote/client/ClientRemoteDBModel.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "client_remote_table")
+@Document(collection = "client_remotes_table")
 public class ClientRemoteDBModel {
 
     @Id
