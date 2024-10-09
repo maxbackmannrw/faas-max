6214a44a9950d5a9ef99aa2357817002e36b1970
maxbackmann <max.backmann@proton.me>
2024-10-09 21:44:49 +0300

    almost finish

--- src/main/java/com/faas/core/api/framework/operation/manager/content/ApiOperationManagerFramework.java
+++ src/main/java/com/faas/core/api/framework/operation/manager/content/ApiOperationManagerFramework.java
@@ -7,14 +7,18 @@ import com.faas.core.api.model.ws.operation.manager.content.dto.ApiOperationActi
 import com.faas.core.api.model.ws.operation.manager.content.dto.ApiOperationManagerWSDTO;
 import com.faas.core.base.model.ws.general.GeneralWSModel;
 import com.faas.core.data.db.operation.content.OperationDBModel;
+import com.faas.core.data.db.user.content.UserDBModel;
 import com.faas.core.data.repo.operation.content.OperationRepository;
+import com.faas.core.data.repo.user.content.UserRepository;
 import com.faas.core.misc.config.AppConstant;
+import com.faas.core.misc.config.AppUtils;
 import com.faas.core.misc.helpers.operation.OperationHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ApiOperationManagerFramework {
@@ -24,6 +28,12 @@ public class ApiOperationManagerFramework {
 
     @Autowired
     OperationRepository operationRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    AppUtils appUtils;
 
 
     public ApiOperationManagerWSDTO apiGetOperationManagerService(long agentId, String operationId) {
@@ -38,7 +48,13 @@ public class ApiOperationManagerFramework {
     public ApiOperationWSDTO apiStartOperationService(long agentId, String operationId) {
 
         List<OperationDBModel> operationDBModels = operationRepository.findByIdAndAgentId(operationId, agentId);
+        Optional<UserDBModel> agentModel = userRepository.findById(agentId);
+        if (!operationDBModels.isEmpty() && agentModel.isPresent()){
+            operationDBModels.get(0).setOperationState(AppConstant.ACTIVE_STATE);
+            operationDBModels.get(0).setuDate(appUtils.getCurrentTimeStamp());
 
+            return new ApiOperationWSDTO(operationRepository.save(operationDBModels.get(0)));
+        }
         return null;
     }
 
--- src/main/java/com/faas/core/api/model/ws/operation/manager/client/dto/ApiOperationClientRemoteWSDTO.java
+++
@@ -1,6 +0,0 @@
-package com.faas.core.api.model.ws.operation.manager.client.dto;
-
-public class ApiOperationClientRemoteWSDTO {
-
-
-}
