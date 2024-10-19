fc231213977099116351dbbaa422299419b793cd
maxbackmann <max.backmann@proton.me>
2024-10-19 22:52:32 +0300

    almost finish

--- src/main/java/com/faas/core/api/framework/operation/manager/campaign/ApiOperationCampaignFramework.java
+++ src/main/java/com/faas/core/api/framework/operation/manager/campaign/ApiOperationCampaignFramework.java
@@ -1,33 +1,33 @@
 package com.faas.core.api.framework.operation.manager.campaign;
 
-import com.faas.core.api.model.ws.campaign.details.dto.ApiCampaignDetailsWSDTO;
 import com.faas.core.api.model.ws.campaign.details.dto.ApiCampaignScenarioWSDTO;
 import com.faas.core.api.model.ws.operation.manager.campaign.dto.ApiOperationCampaignWSDTO;
+import com.faas.core.data.db.campaign.content.CampaignDBModel;
 import com.faas.core.data.db.campaign.content.dao.CampaignScriptDAO;
-import com.faas.core.data.repo.client.details.ClientDetailsRepository;
-import com.faas.core.data.repo.operation.content.OperationRepository;
-import com.faas.core.data.repo.user.content.UserRepository;
+import com.faas.core.data.db.campaign.details.scenario.CampaignScenarioDBModel;
+import com.faas.core.data.repo.campaign.content.CampaignRepository;
+import com.faas.core.data.repo.campaign.details.scenario.CampaignScenarioRepository;
 import com.faas.core.misc.config.AppUtils;
 import com.faas.core.misc.helpers.operation.content.OperationHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class ApiOperationCampaignFramework {
+
 
     @Autowired
     OperationHelper operationHelper;
 
     @Autowired
-    OperationRepository operationRepository;
+    CampaignRepository campaignRepository;
 
     @Autowired
-    ClientDetailsRepository clientDetailsRepository;
-
-    @Autowired
-    UserRepository userRepository;
+    CampaignScenarioRepository campaignScenarioRepository;
 
     @Autowired
     AppUtils appUtils;
@@ -35,21 +35,38 @@ public class ApiOperationCampaignFramework {
 
     public ApiOperationCampaignWSDTO apiGetOperationCampaignService(long agentId, String campaignId) {
 
+        Optional<CampaignDBModel> campaignDBModel = campaignRepository.findById(campaignId);
+        if (campaignDBModel.isPresent()) {
+            return operationHelper.getApiOperationCampaignWSDTO(campaignDBModel.get());
+        }
         return null;
     }
 
     public List<CampaignScriptDAO> apiGetOperationCampaignScriptsService(long agentId, String campaignId) {
 
+        Optional<CampaignDBModel> campaignDBModel = campaignRepository.findById(campaignId);
+        if (campaignDBModel.isPresent() && campaignDBModel.get().getCampaignScripts() != null) {
+            return campaignDBModel.get().getCampaignScripts();
+        }
         return null;
     }
 
     public List<ApiCampaignScenarioWSDTO> apiGetOperationCampaignScenariosService(long agentId, String campaignId) {
 
-        return null;
+        List<ApiCampaignScenarioWSDTO> campaignScenarioWSDTOS = new ArrayList<>();
+        List<CampaignScenarioDBModel> campaignScenarioDBModels = campaignScenarioRepository.findByCampaignId(campaignId);
+        for (CampaignScenarioDBModel campaignScenarioDBModel : campaignScenarioDBModels) {
+            campaignScenarioWSDTOS.add(new ApiCampaignScenarioWSDTO(campaignScenarioDBModel));
+        }
+        return campaignScenarioWSDTOS;
     }
 
     public ApiCampaignScenarioWSDTO apiGetOperationCampaignScenarioService(long agentId, String campaignId, String scenarioId) {
 
+        List<CampaignScenarioDBModel> campaignScenarioDBModels = campaignScenarioRepository.findByCampaignId(campaignId);
+        if (!campaignScenarioDBModels.isEmpty()) {
+            return new ApiCampaignScenarioWSDTO(campaignScenarioDBModels.get(0));
+        }
         return null;
     }
 
