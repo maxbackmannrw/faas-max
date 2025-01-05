dbdd5f5ae577f6cc0719aac54097fc3481ea011d
maxbackmann <max.backmann@proton.me>
2025-01-05 13:49:41 +0300

    almost finish

--- src/main/java/com/faas/core/misc/config/ApiRoute.java
+++ src/main/java/com/faas/core/misc/config/ApiRoute.java
@@ -2,7 +2,6 @@ package com.faas.core.misc.config;
 
 public class ApiRoute {
 
-
     public static final String API_AGENT_LOGIN = "apiagentlogin";
     public static final String API_GET_AGENT_DETAILS = "apigetagentdetails";
     public static final String API_GET_AGENT_SIP_ACCOUNT = "apigetagentsipaccount";
